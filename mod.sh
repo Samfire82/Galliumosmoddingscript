ec6e80388ef4c5da8b4c9bedaed14c24e5bfda89
Samfire82 <96173888+Samfire82@users.noreply.github.com>
2023-06-15 10:33:04 +0100

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -1,50 +1,62 @@
 #!/bin/bash
 clear 
-clear 
 function DialogGen() { 
-dialog --title "For your infomation" --msgbox 'Lets start modding galliumos' 6 20 
+dialog --title "Welcome" --msgbox 'Lets start modding galliumos' 6 20 
 }
 DialogGenfunction DialogGen() { 
-dialog --title "For your infomation" --msgbox 'Lets update repositorys first' 6 20 
+dialog --title "Updates" --msgbox 'Lets update repositorys first' 6 20 
 }
 DialogGen
 sudo apt update -y 
 clear
 function DialogGen() { 
-dialog --title "For your infomation" --msgbox 'Lets start by removing chromium since it has issues' 6 20 
+dialog --title "Add/Remove Software" --msgbox 'Lets start by removing chromium since it has issues' 6 20 
 }
 DialogGen
 sudo apt remove chromium-browser 
 clear 
 function DialogGen() { 
-dialog --title "For your infomation" --msgbox 'Hello GFG Users!' 6 20 
+dialog --title "Add/Remove Software" --msgbox 'Chromium is now gone, lets install FireFox' 6 20 
 }
 DialogGen
 sudo apt install firefox -y
 clear 
 function DialogGen() { 
-dialog --title "For your infomation" --msgbox 'Firefox installed with no errors, lets install a minimal version of plasma' 6 20 
+dialog --title "Add/Remove Software" --msgbox 'FireFox installed with no errors, lets install a minimal version of plasma' 6 20 
 }
 DialogGen
 sudo apt install kde-plasma-desktop
 clear 
 function DialogGen() { 
-dialog --title "For your infomation" --msgbox 'Plasma installed with no errors, lets carry on to Step 4' 6 20 
+dialog --title "Add/Remove Software" --msgbox 'Plasma installed with no errors,' 6 20 
 }
 DialogGen
 clear 
 function DialogGen() { 
-dialog --title "For your infomation" --msgbox 'We need to remove xfce and its packages, you can reinstall xfce but i won't make that script yet' 6 20 
+dialog --title "Add/Remove Software" --msgbox 'We need to remove xfce and its packages, you can reinstall xfce but i won't make that script yet' 6 20 
 }
 DialogGen
 clear 
 sudo apt remove xfce4*
 function DialogGen() { 
-dialog --title "For your infomation" --msgbox 'KDE Plasma and Firefox should be installed. The script will reboot your device for you' 6 20 
+dialog --title "Add/Remove Software" --msgbox 'KDE Plasma and Firefox should be installed. The script will reboot your device for you' 6 20 
 }
 DialogGen
 function DialogGen() { 
-dialog --title "" --msgbox 'Rebooting' 6 20 
+dialog --title "Add/Remove Software" --msgbox '' 6 20 
 }
 DialogGen
-reboot
+function DialogGen() { 
+dialog --title "Distribution Upgrades" --msgbox 'We are going to do a distribution upgrade. Lets hope this works' 6 20 
+}
+DialogGen
+sudo apt upgrade
+ function DialogGen() { 
+dialog --title "Distribution Upgrades" --msgbox 'That hopefully did something... ' 6 20 
+}
+DialogGen 
+function DialogGen() { 
+dialog --title "Finishing Up" --msgbox 'Ok, lets finish up by rebooting and praying if this works' 6 20
+}
+DialogGen
+sudo reboot
