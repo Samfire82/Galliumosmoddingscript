bc9028b8939c9077036db94166d453c6f0844d08
Samfire82 <96173888+Samfire82@users.noreply.github.com>
2023-06-15 12:25:31 +0100

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -13,7 +13,7 @@ function DialogGen() {
 dialog --title "Add/Remove Software" --msgbox 'Lets start by removing chromium since it has issues' 6 20 
 }
 DialogGen
-sudo apt remove chromium-browser 
+sudo apt remove chromium-browser -y
 clear 
 function DialogGen() { 
 dialog --title "Add/Remove Software" --msgbox 'Chromium is now gone, lets install FireFox' 6 20 
@@ -25,7 +25,7 @@ function DialogGen() {
 dialog --title "Add/Remove Software" --msgbox 'FireFox installed with no errors, lets install a minimal version of plasma' 6 20 
 }
 DialogGen
-sudo apt install kde-plasma-desktop
+sudo apt install kde-plasma-desktop -y
 clear 
 function DialogGen() { 
 dialog --title "Add/Remove Software" --msgbox 'Plasma installed with no errors,' 6 20 
@@ -37,7 +37,7 @@ dialog --title "Add/Remove Software" --msgbox 'We need to remove xfce and its pa
 }
 DialogGen
 clear 
-sudo apt remove xfce4*
+sudo apt remove xfce4* -y
 function DialogGen() { 
 dialog --title "Add/Remove Software" --msgbox 'KDE Plasma and Firefox should be installed. The script will reboot your device for you' 6 20 
 }
@@ -50,7 +50,7 @@ function DialogGen() {
 dialog --title "Distribution Upgrades" --msgbox 'We are going to do a distribution upgrade. Lets hope this works' 6 20 
 }
 DialogGen
-sudo apt upgrade
+sudo apt upgrade -y
  function DialogGen() { 
 dialog --title "Distribution Upgrades" --msgbox 'That hopefully did something... ' 6 20 
 }
