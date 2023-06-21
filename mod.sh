657a04e543fc4fc9bbcabb6b912cb07b0f97f388
Samfire82 <96173888+Samfire82@users.noreply.github.com>
2023-06-21 20:03:40 +0100

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo Wow, a new script. lets mod 
+echo Wow a new script. lets mod 
 clear
 read -p "Press any key to continue"
 clear
-echo Right, you want to contiune. Are you sure
+echo Right you want to contiune. Are you sure
 clear
-read -p "If you are really ready, press y to contiune. RFS+PWR to exit"
+read -p "If you are really ready press y to contiune. RFS+PWR to exit"
 clear
 echo Press 1 to contiune
 clear
@@ -13,29 +13,29 @@ read -p "Press 1 to contiune"
 echo Lets start by updating repo's
 sudo apt update -y
 clear 
-echo Repos look good, lets remove xfce4 and its packages
+echo Repos look good lets remove xfce4 and its packages
 clear 
-read -p "Press y to continue, REFRESH+POWER to cancel
+read -p "Press y to continue REFRESH+POWER to cancel
 clear 
-read -p "Are you really sure, if you are press y"
+read -p "Are you really sure if you are press y"
 clear 
 echo Starting removal of xfce4
 clear 
 sudo apt remove xfce4* -y 
 clear
-echo xfce4 gone, lets install kde 
+echo xfce4 gone lets install kde 
 clear
-read -p "Same as before, y to continue"
+read -p "Same as before y to continue"
 clear
 sudo apt install kde-plasma-minimal -y 
 clear
-echo plasma minimal installed,
+echo plasma minimal installed
 clear 
 echo Lets remove chromium and install firefox
 clear 
 read -p "Same as before, press y"
 sudo apt remove chromium-browser -y && sudo apt install firefox -y 
 clear 
-echo We are done! lets reboot, if you get to a terminal, re run the script and report an issue 
+echo We are done! lets reboot if you get to a terminal re run the script and report an issue 
 read -p "Press y to reboot"
 sudo reboot 
